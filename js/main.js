ee036359b287d96c36f0c658a7fab6d184344ad9
alessio <alessio.cirillo.1995@gmail.com>
2020-11-02 16:52:55 +0100

    jsnack 2

--- js/main.js
+++ js/main.js
@@ -1,65 +1,62 @@
-// JSnack 1
-// var biciclette = [
-//     {
-//         nome: 'scott',
-//         peso: 12
-//     },
-//     {
-//         nome: 'bianchi',
-//         peso: 10
-//     },
-//     {
-//         nome: 'giant',
-//         peso: 14
-//     }
-// ];
+//JSnack 1
+var biciclette = [
+    {
+        nome: 'scott',
+        peso: 12
+    },
+    {
+        nome: 'bianchi',
+        peso: 10
+    },
+    {
+        nome: 'giant',
+        peso: 14
+    }
+];
 
-// console.log(biciclette);
+console.log(biciclette);
 
-// var biciLeggera = biciclette[0];
+var biciLeggera = biciclette[0];
 
-// for (var i = 0; i < biciclette.length; i++){
-//     if (biciclette[i].peso < biciLeggera.peso){
-//         biciLeggera = biciclette[i];
-//     }
-// };
+for (var i = 0; i < biciclette.length; i++){
+    if (biciclette[i].peso < biciLeggera.peso){
+        biciLeggera = biciclette[i];
+    }
+};
 
-// console.log(biciLeggera);
+console.log(biciLeggera);
 
-// var source = $("#bici-template").html();
-// var template = Handlebars.compile(source);
+var source = $("#bici-template").html();
+var template = Handlebars.compile(source);
 
-// var data = {
-//     nome: biciLeggera.nome,
-//     peso: biciLeggera.peso
-// }
+var data = {
+    nome: biciLeggera.nome,
+    peso: biciLeggera.peso
+}
 
-// var html = template(data);
-// $('.jsnack1').html(html);
-
-
-// JSnack 2
-// var array = ['alessio', 'marco', 'paolo', 'luigi', 'fabio', 'giorgio'];
-
-// var tot = array.length - 1; 
-// var min = parseInt( prompt( 'inserisci un valore iniziale tra 0 e ' + tot ) );
-// var max = parseInt( prompt( 'inserisci un valore finale tra 0 e ' + tot ) );
+var html = template(data);
+$('.jsnack1').html(html);
 
 
-// var newArray = [];
+//JSnack 2
+var array = ['alessio', 'marco', 'paolo', 'luigi', 'fabio', 'giorgio'];
 
-// for ( var i = 0; i < array.length; i++){
-//     if ( min <= i && max >= i ){
-//         newArray.push(array[i]);
-//     }
-// }
-
-// console.log(array);
-// console.log(newArray);
+var tot = array.length - 1; 
+var min = parseInt( prompt( 'inserisci un valore iniziale tra 0 e ' + tot ) );
+var max = parseInt( prompt( 'inserisci un valore finale tra 0 e ' + tot ) );
 
 
-// JSnack 3
-// Creiamo un array di oggetti (scelti da voi)
-// Creiamo una copia da questo array di partenza e aggiungiamo ai singoli elementi una nuova proprietà position che contiene una lettera casuale.
-// Non dobbiamo modificare l'array iniziale.
+var newArray = [];
+
+for ( var i = 0; i < array.length; i++){
+    if ( min <= i && max >= i ){
+        newArray.push(array[i]);
+    }
+}
+
+console.log(array);
+console.log(newArray);
+
+
+
 
